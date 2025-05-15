01582854f88d3652ba5f3a5a36c34ab1e7a8ddcd
DELL <DELL@PS040>
2025-05-15 16:30:51 +0530

    tc_7 beforetvmore click changed

--- OTT_automation/src/test/java/com/tests/TC_7.java
+++ OTT_automation/src/test/java/com/tests/TC_7.java
@@ -20,9 +20,8 @@ public class TC_7 extends BaseClass{
 		logger.info("Homepage carousels test");
 		
 		loginPage.home();
-		Thread.sleep(2000);
 		scrollUntilElementAppears(homepagecarousels.beforetv,"Before tv");
-		homepagecarousels.morebtnclick(homepagecarousels.beforetvmore,homepagecarousels.beforetvmoreurl(),"Before Tv");
+//		homepagecarousels.morebtnclick(homepagecarousels.beforetvmore,homepagecarousels.beforetvmoreurl(),"Before Tv");
 		
 		scrollUntilElementAppears(homepagecarousels.recentlyadded,"Recently added");
 		homepagecarousels.morebtnclick(homepagecarousels.recentlyaddedmore,homepagecarousels.recentlyaddedmoreurl(),"Recently added");
